602db8b358c9d32921d05a1871ca8941f73d99b0
leeminhhthee <minhthe22122003@gmail.com>
2025-04-25 04:05:48 +0700

    end project

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -24,7 +24,7 @@ export default function MainTabScreen() {
                 size={22}
                 color="gray"
                 style={{ marginHorizontal: 15 }}
-                />
+              />
             </Link>
           ),
         }}
