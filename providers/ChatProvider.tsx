c65d06d325ceeb46ac04a9d1451f5b8122adbaec
leeminhhthee <minhthe22122003@gmail.com>
2025-04-25 02:14:48 +0700

    connect supabase user to steamchat user

--- providers/ChatProvider.tsx
+++ providers/ChatProvider.tsx
@@ -2,21 +2,27 @@ import React, { PropsWithChildren, useEffect, useState } from 'react'
 import { ActivityIndicator } from 'react-native';
 import { StreamChat } from "stream-chat";
 import { Chat, OverlayProvider } from 'stream-chat-expo';
+import { useAuth } from './AuthProvider';
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
 export default function ChatProvider({children}: PropsWithChildren) {
   const [ isReady, setIsReady ] = useState(false)
+  const { profile } = useAuth()
 
   useEffect(() => {
+    if(!profile) {
+      return;
+    }
+    
     const connect = async () => {
       await client.connectUser(
         {
-          id: "jlaheyyy",
-          name: "Jim Lahey",
+          id: profile.id,
+          name: profile.full_name,
           image: "https://i.imgur.com/fR9Jz14.png",
         },
-        client.devToken("jlaheyyy")
+        client.devToken(profile.id)
       );
       setIsReady(true)
 
@@ -30,10 +36,12 @@ export default function ChatProvider({children}: PropsWithChildren) {
     connect();
 
     return () => {
-      client.disconnectUser()
+      if(isReady) {
+        client.disconnectUser()
+      }
       setIsReady(false)
     }
-  }, []);
+  }, [profile?.id]);
 
   if(!isReady) {
     return <ActivityIndicator />
